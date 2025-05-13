e499bb4adad0b0fd50929d3bffc347f1af72f36a
Your Name <you@example.com>
2025-05-13 18:19:27 +0100

    Fix: use Pact mockService.baseUrl instead of hardcoded port

--- test/consumer.test.js
+++ test/consumer.test.js
@@ -13,32 +13,32 @@ const provider = new Pact({
 });
 
 describe("Pact with provider", () => {
-  beforeAll(() =>
-    provider.setup().then(() => {
-      provider.addInteraction({
-        state: "provider returns sum",
-        uponReceiving: "a GET request to /api/add?a=1&b=2",
-        withRequest: {
-          method: "GET",
-          path: "/api/add",
-          query: {
-            a: "1",
-            b: "2",
-          },
+  beforeAll(async () => {
+    await provider.setup();
+    await provider.addInteraction({
+      state: "provider returns sum",
+      uponReceiving: "a GET request to /api/add?a=1&b=2",
+      withRequest: {
+        method: "GET",
+        path: "/api/add",
+        query: {
+          a: "1",
+          b: "2",
         },
-        willRespondWith: {
-          status: 200,
-          headers: { "Content-Type": "application/json" },
-          body: {
-            result: Matchers.like(3),
-          },
+      },
+      willRespondWith: {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+        body: {
+          result: Matchers.like(3),
         },
-      });
-    })
-  );
+      },
+    });
+  });
 
   it("should return a successful body", async () => {
-    const res = await axios.get("http://localhost:1234/api/add?a=1&b=2");
+    const url = `${provider.mockService.baseUrl}/api/add?a=1&b=2`;
+    const res = await axios.get(url);
     expect(res.status).toEqual(200);
     expect(res.data.result).toEqual(3);
   });
